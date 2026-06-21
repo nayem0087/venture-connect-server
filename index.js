8d2becfd7119bf79c1391879396e3d6c3761a419
Nayem Ahmed <nayemk0087@gmail.com>
2026-06-21 16:18:54 +0600

    added startups post collection in mongodb

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 5000;
 require('dotenv').config();
 
+app.use(cors());
+app.use(express.json());
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 
@@ -15,28 +19,38 @@ const uri = process.env.MONGO_DB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try { 
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("venture_connect_db");
+        const startupCollection = database.collection("startups");
+
+        app.post('/api/startups', async(req, res) => {
+            const startup = req.body;
+            const result = await startupCollection.insertOne(startup);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
